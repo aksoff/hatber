1c08ccbae420cc2c56bcc20bfda86769249caf2d
aksoff <alexn.popoff@gmail.com>
2020-10-13 01:43:33 +0400

    keys

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://hatber:jLv77W6ZkGjWvU9z@cluster0.onkjb.gcp.mongodb.net/hatber?retryWrites=true&w=majority',
+    jwt: 'aks-jwt',
+    tokenExpired: 3600
+}
