88307583f6afed38a2193c84da313e8ebd512b61
aksoff <alexn.popoff@gmail.com>
2020-10-12 22:58:08 +0400

    mongoDB configure

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const authRoutes = require('./routes/auth')
+const keys = require('./config/keys')
 
 const app = express()
+
+mongoose
+  .connect(keys.mongoURI, { 
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    })
+  .then(() => console.log('MongoDB connected.'))
+  .catch((error) => console.log(error))
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use('/api/auth', authRoutes)
 
 module.exports = app
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://hatber:jLv77W6ZkGjWvU9z@cluster0.onkjb.gcp.mongodb.net/hatber?retryWrites=true&w=majority'
+}
