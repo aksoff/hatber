8dba36603eadafda4bca364364b3a3cd2ec0b380
aksoff <an_popoff@inbox.ru>
2020-10-30 15:30:32 +0400

    spinner

--- client/src/app/performers-page/performers-page.component.ts
+++ client/src/app/performers-page/performers-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
 import { Performer } from '../shared/services/interfaces'
 import { PerformersService } from '../shared/services/performers.service'
 
@@ -10,12 +11,20 @@ import { PerformersService } from '../shared/services/performers.service'
 })
 export class PerformersPageComponent implements OnInit {
   displayedColumns: string[] = ['Idx', 'name', 'action']
-  dataSource: Observable<Performer[]>
-  spinner = false
+  dataSource: Performer[]
+  spinner = true
   constructor(private performersService: PerformersService) {}
 
   ngOnInit(): void {
-    this.dataSource = this.performersService.fetch()
-    this.spinner = true
+    // this.dataSource = this.performersService.fetch()
+    // this.spinner = true
+
+    this.performersService.fetch().subscribe(
+      (data) => {
+        this.spinner = false
+        this.dataSource = data
+      },
+      (error) => (this.spinner = false)
+    )
   }
 }
