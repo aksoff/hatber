a81b186c096c0dc268e4ae4e78563a204ca9b7d3
aksoff <an_popoff@inbox.ru>
2020-11-09 15:53:06 +0400

    fix order model

--- models/Order.js
+++ models/Order.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const OrderSchema = new Schema({
+const orderSchema = new Schema({
   date: {
     type: Date,
     default: Date.now
